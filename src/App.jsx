65c912019111e349aa657ab8b272ab4bda7ec399
Aarni <aarni.fangmayer@gmail.com>
2026-02-06 00:55:12 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>hello</h1>
+      <h2>hello</h2>
+      <h3>hello</h3>
+      <p className="subtitle">i am text subtitle</p>
+      <p>i am usual paragraph</p>
+    </>
+  )
+}
+
+export default App
