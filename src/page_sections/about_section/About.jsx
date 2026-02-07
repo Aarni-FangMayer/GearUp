0a4582f9922b00cad3a76569cc32038e1279f76a
Aarni <aarni.fangmayer@gmail.com>
2026-02-07 04:20:22 +0200

    feat: Implement and fully adapt About section for all devices

--- src/page_sections/about_section/About.jsx
+++ src/page_sections/about_section/About.jsx
@@ -1,12 +1,48 @@
-// import React from 'react'
-// import './About.css'
+import React from "react";
+import "./About.css";
+import aboutImage from "../../assets/img/about_image.jpg";
 
-// const About = () => {
-//   return (
-//     <div>
-//       this is about section
-//     </div>
-//   )
-// }
+import Highlight from "../../components/ui/highlight_item/Highlight";
+import Button from "../../components/buttons/base_button/Button";
 
-// export default About
+const About = ({ id }) => {
+  return (
+    <section className="about-section" id={id}>
+      <div className="about-section__content base-grid">
+        <img
+          className="about-section__image"
+          src={aboutImage}
+          alt="Changing car oil"
+        />
+        <div className="about-section__text">
+          <div className="about-section__titles">
+            <h3 className="about-section__title">About GearUp</h3>
+            <div className="about-section__subtitle subtitle">
+              Professional auto service you can trust
+            </div>
+          </div>
+          <p className="about-section__description">
+            GearUp is a full-service auto repair shop built on precision,
+            expertise, and transparency. We combine skilled technicians with
+            advanced diagnostics to deliver reliable results without unnecessary
+            work.{" "}
+          </p>
+          <div className="about-section__highlits">
+            <Highlight number="15+" text="Years Experience" />
+            <Highlight number="34" text="Services Available" />
+            <Highlight number="100%" text="Satisfaction Guaranteed" />
+          </div>
+          <div className="about-section__button">
+            <Button
+              priority="primary"
+              text="See All Services"
+              onClick={() => console.log("primary clicked")}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import MobileMenu from '../components/navigation/mobilemenu/MobileMenu';
 import Hero from '../page_sections/hero_section/Hero';
 import PromoServices from '../page_sections/promo_section/PromoServices';
-// import About from '../page_sections/about_section/About';
+import About from '../page_sections/about_section/About';
 // import Trust from '../page_sections/trust_section/Trust';
 // import Services from '../page_sections/services_section/Services';
 // import Contacts from '../page_sections/contacts_section/Contacts';
@@ -14,13 +14,14 @@ const Home = () => {
         <MobileMenu />
         <Hero id="hero" />
         <PromoServices id="promo" />
+        <About id="about" />
         
     {/* <section id="promo">
         
     </section> */}
       
     {/* <section id="about">
-        <About />
+        
     </section> */}
     
     {/* <section id="trust">
